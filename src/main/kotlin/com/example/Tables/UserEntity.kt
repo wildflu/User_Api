697f1f9a99aa2622c8d5bae6f5e7aaf6b2ad37a5
wildflu <walidcode0@gmail.com>
2023-03-03 16:53:22 +0100

    Entity user(table in db)

---
+++ src/main/kotlin/com/example/Tables/UserEntity.kt
@@ -0,0 +1,12 @@
+package com.example.Tables
+
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+object UserEntity: Table<Nothing>("users") {
+    val id = int("id").primaryKey()
+    val login = varchar("login")
+    val password = varchar("password")
+    val role = varchar("role")
+}
